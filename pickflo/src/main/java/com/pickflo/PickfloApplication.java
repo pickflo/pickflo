31eae9ea527a474d2b09d667391ee9c77176a141
Jeonghee Lee <tpqms316@naver.com>
2024-08-22 18:10:58 +0900

    초기설정

---
+++ pickflo/src/main/java/com/pickflo/PickfloApplication.java
@@ -0,0 +1,13 @@
+package com.pickflo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickfloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickfloApplication.class, args);
+	}
+
+}
