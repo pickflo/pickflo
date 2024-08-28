28e6cc0c969219e31949cc453591e5e4744ee8a5
hyun0618 <jhh48654@gmail.com>
2024-08-28 17:39:54 +0900

    search

--- pickflo/src/main/java/com/pickflo/web/SearchController.java
+++ pickflo/src/main/java/com/pickflo/web/SearchController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SearchController {
 	
-	@GetMapping("/search")
+	@GetMapping("/movie/search")
 	public void search() {
 		
 	}
